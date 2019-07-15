cf260b69606afb97365a1a1bdcb27cf9a71098cb
Hari <ofc23hari@gmail.com>
2019-07-15 20:27:36 +0530

    change the git

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD28Nr4Izy4fDjDyXEF_KqEUDlcIRPyJzQ",
+    authDomain: "fir-crud-d3b4a.firebaseapp.com",
+    databaseURL: "https://fir-crud-d3b4a.firebaseio.com",
+    projectId: "fir-crud-d3b4a",
+    storageBucket: "",
+    messagingSenderId: "623467232507"
+    // appId: "1:623467232507:web:1ad9e4d20fb04284"
+  }
+};
